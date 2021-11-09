9c932cedd3abd2e3f036d571e4b3ff0b36c36ef6
JennyViannay <jenny.viannay75@gmail.com>
2021-11-09 08:16:50 +0100

    start here

--- routes/articles.js
+++ routes/articles.js
@@ -13,51 +13,24 @@ router.get('/', (req, res) => {
 
 // GET ONE
 router.get('/:id', (req, res) => {
-    connection.query(`SELECT * FROM article WHERE id = ${Number(req.params.id)}`, (err, result) => {
-        if (err) res.status(500).send(`Erreur lors de la récupération de l\'article !!`);
-        else if (result == "") res.status(404).send('Not found');
-        else res.status(200).send(result);
-    });
-});
-
-// CREATE
-router.post('/', (req, res) => {
-    const data = [
-        String(req.body.title),
-        String(req.body.content),
-        String(req.body.image),
-        Number(req.body.price),
-        Number(req.params.id)
-    ];
-    connection.query(`INSERT INTO article (title, content, image) VALUES (?, ?, ?)`, data,
-        (err, result) => {
-            if (err) res.status(500).send(`Erreur lors de la création de l\'article !! ${err}`);
-            else res.status(200).send(result);
-        });
-});
-
-// UPDATE
-router.post('/:id', (req, res) => {
-    const data = [
-        String(req.body.title),
-        String(req.body.content),
-        String(req.body.image),
-        Number(req.params.id)
-    ];
-    connection.query(`UPDATE article SET title = ?, content = ?, image = ? WHERE id = ?`, data,
-        (err, result) => {
-            if (err) res.status(500).send(`Erreur lors de la modification de l\'article !!`)
-            else res.status(200).send(result)
-        });
+    // TODO:: you code here
 });
 
 // DELETE
 router.delete('/:id', (req, res) => {
-    connection.query(`DELETE FROM article WHERE id = ${Number(req.params.id)}`,
-        (err, result) => {
-            if (err) res.status(500).send(`Erreur lors de la suppression de l\'article !!`)
-            else res.status(200).send(result)
-        });
+    // TODO:: you code here
 });
+
+// CREATE
+router.post('/', (req, res) => {
+    // TODO:: BONUS: you code here
+});
+
+// UPDATE
+router.post('/:id', (req, res) => {
+    // TODO:: BONUS: you code here
+});
+
+
 
 export default router;
